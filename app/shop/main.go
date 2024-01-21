6a2e0790d5103f6a6d57d26a2c586e5ed650a65e
echo music <php_fangting@126.com>
2024-01-21 21:46:09 +0800

    make file

--- app/shop/main.go
+++ app/shop/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
+	"github.com/casdoor/casdoor-go-sdk/casdoorsdk"
 	"github.com/gin-gonic/gin"
-	"github.com/go-resty/resty/v2"
 )
+
+func init() {
+	content, err := ioutil.ReadFile("./token_jwt_key.pem")
+	if err != nil {
+		panic(err)
+	}
+	var certificate = string(content)
+
+	casdoorsdk.InitConfig("http://localhost:8000", "72c162ebfb9a7f597be9", "cad87299a9f59861010dec3e91d99512b7b210a5", certificate, "organization_ep5s2s", "shop")
+}
 
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -48,31 +57,41 @@ type CallbackResp struct {
 }
 
 func callbackHandle(c *gin.Context) {
-	client := resty.New()
+	// client := resty.New()
 
-	var req = make(map[string]string, 5)
-	req["grant_type"] = "authorization_code"
-	req["client_id"] = "72c162ebfb9a7f597be9"
-	req["client_secret"] = "cad87299a9f59861010dec3e91d99512b7b210a5"
-	req["code"] = c.Query("code")
+	// var req = make(map[string]string, 5)
+	// req["grant_type"] = "authorization_code"
+	// req["client_id"] = "72c162ebfb9a7f597be9"
+	// req["client_secret"] = "cad87299a9f59861010dec3e91d99512b7b210a5"
+	// req["code"] = c.Query("code")
 
-	resp, err := client.R().
-		SetHeader("Content-Type", "application/json").
-		SetBody(req).
-		Post("http://localhost:8000/api/login/oauth/access_token")
+	// resp, err := client.R().
+	// 	SetHeader("Content-Type", "application/json").
+	// 	SetBody(req).
+	// 	Post("http://localhost:8000/api/login/oauth/access_token")
+	// if err != nil {
+	// 	panic("hello")
+	// }
+	// var res CallbackResp
+
+	// json.Unmarshal([]byte(resp.String()), &res)
+	token, err := casdoorsdk.GetOAuthToken(c.Query("code"), c.Query("state"))
 	if err != nil {
-		panic("hello")
+		panic(err)
 	}
-	var res CallbackResp
-	fmt.Println(resp.String())
 
-	json.Unmarshal([]byte(resp.String()), &res)
+	claims, err := casdoorsdk.ParseJwtToken(token.AccessToken)
+	if err != nil {
+		panic(err)
+	}
+
+	claims.AccessToken = token.AccessToken
 
 	// 处理 GET 请求
 	c.JSON(http.StatusOK, gin.H{
 		"code":  c.Query("code"),
 		"state": c.Query("state"),
-		"resp":  res,
+		"resp":  claims.User,
 	})
 }
 func listHandle(c *gin.Context) {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module casdoor-deploy
 go 1.21.3
 
 require (
+	github.com/casdoor/casdoor-go-sdk v0.35.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-resty/resty/v2 v2.11.0
 )
@@ -16,6 +17,8 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
@@ -30,8 +33,10 @@ require (
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/oauth2 v0.13.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect

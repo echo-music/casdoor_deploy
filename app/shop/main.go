3e1a7ea0033fa26e8d290e6e151552fb39460a71
echo music <php_fangting@126.com>
2024-01-21 20:54:18 +0800

    make file

--- app/shop/main.go
+++ app/shop/main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-)
-
-var (
-	endpoint         = "http://localhost:8000"
-	clientId         = "72c162ebfb9a7f597be9"
-	clientSecret     = "cad87299a9f59861010dec3e91d99512b7b210a5"
-	organizationName = "organization_ep5s2s"
-	applicationName  = "shop"
+	"github.com/go-resty/resty/v2"
 )
 
 func Cors() gin.HandlerFunc {
@@ -44,19 +39,43 @@ func main() {
 	log.Fatal(router.Run(":8080"))
 }
 
+type CallbackResp struct {
+	AccesToken   string `json:"access_token"`
+	IdToken      string `json:"id_token"`
+	RefreshToken string `json:"refresh_token"`
+	TokenType    string `json:"token_type"`
+	ExpiresIn    int    `json:"expires_in"`
+}
+
 func callbackHandle(c *gin.Context) {
-	
-	
-  
-	
+	client := resty.New()
+
+	var req = make(map[string]string, 5)
+	req["grant_type"] = "authorization_code"
+	req["client_id"] = "72c162ebfb9a7f597be9"
+	req["client_secret"] = "cad87299a9f59861010dec3e91d99512b7b210a5"
+	req["code"] = c.Query("code")
+
+	resp, err := client.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(req).
+		Post("http://localhost:8000/api/login/oauth/access_token")
+	if err != nil {
+		panic("hello")
+	}
+	var res CallbackResp
+	fmt.Println(resp.String())
+
+	json.Unmarshal([]byte(resp.String()), &res)
+
 	// 处理 GET 请求
 	c.JSON(http.StatusOK, gin.H{
 		"code":  c.Query("code"),
 		"state": c.Query("state"),
+		"resp":  res,
 	})
 }
 func listHandle(c *gin.Context) {
-
 	// 处理 GET 请求
 	c.JSON(http.StatusUnauthorized, gin.H{
 		"code":       401,
